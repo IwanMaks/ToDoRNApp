12c4b911510bacc0c3ad6a00737ab5a269d08307
Anastasiia <zianastasia@yandex.ru>
2021-05-02 09:50:40 +0300

    Add remote profile img import

--- src/components/ProfileScreen/ProfileAvatar.js
+++ src/components/ProfileScreen/ProfileAvatar.js
@@ -1,18 +1,17 @@
 import React from "react";
-import {StyleSheet, View, Text, TouchableOpacity} from 'react-native'
+import {StyleSheet, View, Text, TouchableOpacity, Image} from 'react-native'
 import {EditIcon} from "../../ui/icons/EditIcon";
 import {THEME} from "../../theme/theme";
 
 export const ProfileAvatar = () => {
     return (
         <View style={styles.avatarWrap}>
-            <View style={styles.imgWrap}>
-                <TouchableOpacity activeOpacity={0.8}>
-                    <View style={styles.editWrap}>
-                        <EditIcon />
-                    </View>
-                </TouchableOpacity>
-            </View>
+            <Image source = {{ uri: 'https://sun9-34.userapi.com/impg/tD8tE2BqXBO0IRoLad70hwIsZgGeftmkvC0eQg/iplAEXKnHh0.jpg?size=1620x2160&quality=96&sign=0cadfa3d6f144d1035cd4721cdb39fab&type=album' }} style={styles.imgWrap}/>
+            <TouchableOpacity activeOpacity={0.8} style={styles.edit}>
+                <View style={styles.editWrap}>
+                    <EditIcon />
+                </View>
+            </TouchableOpacity>
             <Text style={styles.userName}>Albert Tino</Text>
             <Text style={styles.userProf}>UI/UX Designer</Text>
         </View>
@@ -27,13 +26,16 @@ const styles = StyleSheet.create({
     imgWrap: {
         width: 120,
         height: 120,
-        backgroundColor: THEME.ascent1_dark,
         borderRadius: 60,
         borderColor: THEME.ascent2_dark,
         borderWidth: 3,
         alignItems: 'flex-end',
         justifyContent: 'flex-end',
         top: -60
+    },
+    edit: {
+        top: -103,
+        left: 40
     },
     editWrap: {
         width: 42,
