63322e7e491c9f46fbb7f1846a8e14d2db53747a
Iwan <iwan.maksackov@yandex.ru>
2021-05-18 21:35:38 +0300

    fix bug in month

--- src/secondary.js
+++ src/secondary.js
@@ -15,7 +15,7 @@ export function formatTime() {
         'December',
     ];
 
-    return month[date.getMonth() - 1] + ' ' + addZero(date.getDate()) + ', ' + date.getFullYear()
+    return month[date.getMonth()] + ' ' + addZero(date.getDate()) + ', ' + date.getFullYear()
 }
 
 const addZero = (n) => n < 10 ? '0' + n : n
